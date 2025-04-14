e3bb77894185dc2c3353b9ef078601b7eaae9d8c
PioKl <kosiu342@gmail.com>
2025-04-14 19:40:58 +0200

    Display of Testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "./components/Hero";
 import PropertiesShow from "./components/PropertiesShow";
+import Testimonials from "./components/Testimonials";
 export default function Home() {
   return (
     <>
       <Hero />
       <PropertiesShow />
+      <Testimonials />
     </>
   );
 }
